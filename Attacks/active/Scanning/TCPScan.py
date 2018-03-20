533745a3f746e0ad390c4095bd6213673c105e61
H4ck-80T <31415511+H4ck-80T@users.noreply.github.com>
2018-03-20 15:08:29 +0530

    Update TCPScan.py

--- Attacks/active/Scanning/TCPScan.py
+++ Attacks/active/Scanning/TCPScan.py
@@ -1,10 +1,12 @@
-from socket import *
+import socket
 import sys
 
 addr=gethostbyname(sys.argv[1])
-sock = socket(AF_INET,SOCK_STREAM)
+sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 for i in range(0,65535):
 	err=sock.connect_ex((addr,i))
 	if err == 0:
 		print "TCP port " + str(i) + " open"
+		sock.close()
+		sock = socket.socket(socket.AF_INET,sock.SOCK_STREAM)
