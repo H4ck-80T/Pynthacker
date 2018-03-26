c5eb5a6cb7b02e31f726587affc8c95758d13b88
H4ck-80T <31415511+H4ck-80T@users.noreply.github.com>
2018-03-27 01:24:41 +0530

    Update TCPScan.py

--- Attacks/active/Scanning/TCPScan.py
+++ Attacks/active/Scanning/TCPScan.py
@@ -2,9 +2,10 @@ import socket
 import sys
 
 addr=socket.gethostbyname(sys.argv[1])
-sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 for i in range(0,65535):
+	sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        socket.setdefaulttimeout(2)
 	err=sock.connect_ex((addr,i))
 	if err == 0:
 		print "TCP port " + str(i) + " open"
